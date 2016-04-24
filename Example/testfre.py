39384bfe3f5da73b1cf45991ac664ba71c85c998
Henghui Zhu <zhhtc200@gmail.com>
2016-04-24 11:33:47 -0400

    Finish mnist

--- Example/testfre.py
+++ Example/testfre.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import scipy.io
+import gensim
+import numpy as np
 
 mat = scipy.io.loadmat('../Data/data_mnist_test.mat')
 
@@ -14,3 +16,11 @@ for doc in Test:
         if doc[i] != 0:
             corpus_item.append((i,doc[i]))
     mycorpus.append(corpus_item)
+
+print('Hello')
+
+lda = gensim.models.ldamodel.LdaModel(corpus=mycorpus, num_topics=100, update_every=0, passes=1)
+Beta = lda.state.get_lambda()
+Beta = np.array(Beta)
+
+scipy.io.savemat('../Data/MNIST_Test.mat', {'Beta':Beta})
