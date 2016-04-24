f67d8fafd03d5e553d3abe9f2ff5273c89e8efe3
Henghui Zhu <zhhtc200@gmail.com>
2016-04-24 12:35:38 -0400

    repair bugs in mnist

--- Example/Test_MNIST_Test.py
+++ Example/Test_MNIST_Test.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+
 import scipy.io
 import gensim
 import numpy as np
 
-mat = scipy.io.loadmat('../Data/data_mnist_train.mat')
+mat = scipy.io.loadmat('../Data/data_mnist_test.mat')
 
 Test = mat['X_test']
 
@@ -13,14 +14,13 @@ mycorpus = []
 for doc in Test:
     corpus_item = []
     for i in range(784):
-        if doc[i] != 0:
-            corpus_item.append((i,doc[i]))
+        corpus_item.append((i,doc[i]))
     mycorpus.append(corpus_item)
 
 print('Hello')
 
-lda = gensim.models.ldamodel.LdaModel(corpus=mycorpus, num_topics=100, update_every=0, passes=10)
+lda = gensim.models.ldamodel.LdaModel(corpus=mycorpus, num_topics=100, update_every=0, passes=1)
 Beta = lda.state.get_lambda()
 Beta = np.array(Beta)
 
-scipy.io.savemat('../Data/MNIST_Train.mat', {'Beta':Beta})
+scipy.io.savemat('../Data/MNIST_Test.mat', {'Beta':Beta})
--- Example/Test_MNIST_Train.py
+++ Example/Test_MNIST_Train.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
-
 import scipy.io
 import gensim
 import numpy as np
 
-mat = scipy.io.loadmat('../Data/data_mnist_test.mat')
+mat = scipy.io.loadmat('../Data/data_mnist_train.mat')
 
-Test = mat['X_test']
+Test = mat['X_train']
 
 mycorpus = []
 
 for doc in Test:
     corpus_item = []
     for i in range(784):
-        if doc[i] != 0:
-            corpus_item.append((i,doc[i]))
+        corpus_item.append((i,doc[i]))
     mycorpus.append(corpus_item)
 
 print('Hello')
 
-lda = gensim.models.ldamodel.LdaModel(corpus=mycorpus, num_topics=100, update_every=0, passes=1)
+lda = gensim.models.ldamodel.LdaModel(corpus=mycorpus, num_topics=100, update_every=0, passes=10)
 Beta = lda.state.get_lambda()
 Beta = np.array(Beta)
 
-scipy.io.savemat('../Data/MNIST_Test.mat', {'Beta':Beta})
+scipy.io.savemat('../Data/MNIST_Train.mat', {'Beta':Beta})
